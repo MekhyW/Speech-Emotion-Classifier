d358e94f4d677369439bc91865946ccb078baf2f
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-08-21 17:20:11 -0300

    Eliminated wavs with failed resampling

--- erase_corrupted_wavs.py
+++ erase_corrupted_wavs.py
@@ -1,19 +1,27 @@
 import wave
 import os
+import tensorflow as tf
 
 corrupted = []
 
 for set in ['test', 'train']:
     for folder in os.listdir(set):
         for file in os.listdir(set+'/'+folder):
-            try:
-                opened = wave.open(set+'/'+folder+'/'+file, 'rb')
-                if opened.getnchannels() != 1:
-                    corrupted.append(set+'/'+folder+'/'+file)
-                opened.close()
-            except:
-                corrupted.append(set+'/'+folder+'/'+file)
+            path = set+'/'+folder+'/'+file
+            if os.path.getsize(path) == 0 or not file.endswith('.wav'):
+                corrupted.append(path)
+            else:
+                try:
+                    opened = wave.open(path, 'rb')
+                    if opened.getnchannels() != 1:
+                        corrupted.append(path)
+                    opened.close()
+                    input_data = tf.io.read_file(path)
+                    audio, sampling_rate = tf.audio.decode_wav(input_data)
+                except:
+                    corrupted.append(path)
 
 for path in corrupted:
     os.remove(path)
     print(path+' removed')
+print(len(corrupted), 'files removed')
